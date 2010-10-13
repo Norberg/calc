d04c107c0c3a4488f2bdd45ab5055a61fbcfcae2
Filip Andersson <flpandersson@gmail.com>
2010-10-13 15:17:51 +0200

    Added parameter check
    Added check to see if the file submitted as parameter is a .calc file. Should make things easier for "some people"

--- i836-driver.sh
+++ i836-driver.sh
@@ -3,16 +3,25 @@
 
 # Print error-message
 if [ $# -le 0 ]; then
-	echo ".calc-file input needed."
+	echo "ERROR: .calc-file input needed."
+	exit
+fi
+if [ "`echo "$1" | cut -d"." -f2`" != "calc" ]; then
+	echo "ERROR: .calc-file input needed."
+	exit
 fi
 
+# Create a proper filename for the compiled file
 fileName=`echo $1 |cut -d'.' -f1`
 
+# Create a .s filename for the file containing the assembly code
 assemblyFile=$fileName.s
 
+# Pipe the contents of prologue.s, the file compiled with calc3i and epilogue.s to the assembly file
 cat prologue.s > $assemblyFile
 ./calc3i < $1 >> $assemblyFile
 cat epilogue.s >> $assemblyFile
 
+# Compile the assembly file with gcc
 gcc $assemblyFile -o$fileName
 
