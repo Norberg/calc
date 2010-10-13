0006eda361ee783e100f6294bd26cc588526409a
Filip Andersson <flpandersson@gmail.com>
2010-10-13 15:24:46 +0200

    Prologue ABC-support
    prologue.s now supports the entire english alphabet!

--- prologue.s
+++ prologue.s
@@ -5,6 +5,28 @@ a:	.skip	4
 b:	.skip	4
 c:	.skip	4
 d:	.skip	4
+e:	.skip	4
+f:	.skip	4
+g:	.skip	4
+h:	.skip	4
+i:	.skip	4
+j:	.skip	4
+k:	.skip	4
+l:	.skip	4
+m:	.skip	4
+n:	.skip	4
+o:	.skip	4
+p:	.skip	4
+q:	.skip	4
+r:	.skip	4
+s:	.skip	4
+t:	.skip	4
+u:	.skip	4
+v:	.skip	4
+w:	.skip	4
+x:	.skip	4
+y:	.skip	4
+z:	.skip	4
 FINT:	.string "%d"
 	.text
 	.global main
