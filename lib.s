711573c1351b0f470d60d1687cdfc3e5b1dfef51
Filip Andersson <flpandersson@gmail.com>
2010-10-20 14:33:02 +0200

    gcd function added
    Added a function which calculates gcd. Also made some improvements to the fact function so that it now calculates the correct factorial of 0

--- lib.s
+++ lib.s
@@ -12,9 +12,16 @@ FINT:	.string "%d\n"
 fact:
 	movl 	4(%esp), %ecx
 	movl 	$1, %eax
+	cmpl	%eax, %ecx
+	jl	l7
 l1:	mull 	%ecx
 	loop 	l1
-	ret
+	jmp	l8
+l7:	movl	$1, %ecx
+l8:	ret
+
+/* lntwo */
+
 lntwo:
 	movl	4(%esp), %ecx
 	movl	$1, %eax
@@ -26,11 +33,33 @@ l2:	cmpl	%eax, %ecx
 	jmp	l2	
 l3:	movl	%ebx, %eax
 	ret
-	
+
+/* gcd */
+
+gcd:
+      	movl 	4(%esp), %eax
+      	movl 	8(%esp), %ebx
+l4:   	cmp 	%eax, %ebx 
+      	je  	l6      
+      	jle 	l5       
+      	sub 	%eax, %ebx 
+      	jmp 	l4       
+l5:  	sub  	%ebx, %eax 
+      	jmp 	l4       
+l6:
+      ret            
+
 
 main: 
-	pushl 	$64
-	call 	lntwo
+	/*
+	pushl 	$36
+	pushl	$24
+	call 	gcd
+	pushl 	%ebx
+	push 	$FINT
+	call 	printf */
+	pushl 	$4
+	call	fact
 	pushl 	%eax
 	push 	$FINT
 	call 	printf
