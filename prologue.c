e3a8f844a3dd305c8616e6087f0048781499e011
Filip Andersson <flpandersson@gmail.com>
2010-10-21 14:38:18 +0200

    Stack functionality
    Added stack functions to prologue.c

--- prologue.c
+++ prologue.c
@@ -1,3 +1,26 @@
 #include<stdio.h>
 
+//Stack functions
+void push(int top, int *stack, int *current)
+{
+	stack[*current+1] = top;
+	*current = *current + 1;
+}
+int pop(int *stack, int *current) 
+{
+	int temp = -1;
+	
+	if(*current <= 0) 
+	{
+		printf("Fail, there is no content on the stack that can be pop'ed \n");
+	}
+	else 
+	{
+		temp = stack[*current];
+		*current = *current - 1;
+	}
+
+	return temp;
+}
+
 int main() {
